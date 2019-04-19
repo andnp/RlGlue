2abb319b8976b7b09649fd61dee418be2a849999
Travis CI User <travis@example.org>
2019-04-19 19:14:23 +0000

    updating to version 0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='andnpatterson@gmail.com',
     packages=find_packages(exclude=['tests*']),
     install_requires=[],
-    version=0.1,
+    version=0.2,
     license='MIT',
     description='A tiny re-implementation of the rl-glue interface',
     long_description='todo',

b662a0cb8512c1da70a65fca26a41e36abdd369e
andy <andnpatterson@gmail.com>
2019-04-19 12:58:05 -0600

    fix setup.py version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='andnpatterson@gmail.com',
     packages=find_packages(exclude=['tests*']),
     install_requires=[],
-    version=0.5,
+    version=0.0,
     license='MIT',
     description='A tiny re-implementation of the rl-glue interface',
     long_description='todo',

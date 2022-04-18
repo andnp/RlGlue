6b88ae69992593df49765e4e80d17b516882bdc2
github-action <andnpatterson@gmail.com>
2022-04-18 02:48:36 +0000

    bump: version 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -4,17 +4,28 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name='RlGlue-andnp',
-    url='https://github.com/andnp/RlGlue.git',
-    author='Andy Patterson',
-    author_email='andnpatterson@gmail.com',
-    packages=find_packages(exclude=['tests*']),
-    version='0.0.4',
-    license='MIT',
-    description='A tiny re-implementation of the rl-glue interface',
+    name="RlGlue-andnp",
+    url="https://github.com/andnp/RlGlue.git",
+    author="Andy Patterson",
+    author_email="andnpatterson@gmail.com",
+    packages=find_packages(exclude=["tests*"]),
+    version="0.0.5",
+    license="MIT",
+    description="A tiny re-implementation of the rl-glue interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.6",
     install_requires=[],
-    extras_require={},
+    extras_require={
+        "dev": [
+            "numpy>=1.19.5",
+            "flake8",
+            "commitizen",
+            "pre-commit",
+            "pipenv-setup[black]",
+            "build",
+            "twine",
+            "mypy",
+        ],
+    },
 )

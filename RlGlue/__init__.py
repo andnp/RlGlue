c412d7360efa3309c50399c230a647d41cfa7c0b
andy <andnpatterson@gmail.com>
2022-11-01 21:32:34 -0600

    fix: make interface type-checkable

--- RlGlue/__init__.py
+++ RlGlue/__init__.py
@@ -1,3 +1,3 @@
-from RlGlue.rl_glue import RlGlue
-from RlGlue.agent import BaseAgent
-from RlGlue.environment import BaseEnvironment
+from RlGlue.rl_glue import RlGlue as RlGlue # NOQA: F401
+from RlGlue.agent import BaseAgent as BaseAgent # NOQA: F401
+from RlGlue.environment import BaseEnvironment as BaseEnvironment # NOQA: F401

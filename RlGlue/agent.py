bec78c4e5830e69e1abf3b86d3e342e6facf5a6e
andnp <andnpatterson@gmail.com>
2025-03-09 20:04:55 -0600

    feat: modernize API
    BREAKING CHANGE: this changes all api contracts to more closely match
    the gymnasium api.

--- RlGlue/agent.py
+++ RlGlue/agent.py
@@ -1,16 +1,16 @@
 from abc import abstractmethod
-from typing import Any, Dict
+from typing import Any, Protocol
 
 
-class BaseAgent:
+class BaseAgent(Protocol):
     @abstractmethod
-    def start(self, observation: Any) -> int:
+    def start(self, obs: Any) -> int:
         raise NotImplementedError('Expected `start` to be implemented')
 
     @abstractmethod
-    def step(self, reward: float, observation: Any, extra: Dict[str, Any]) -> int:
+    def step(self, reward: float, obs: Any, extra: dict[str, Any]) -> int:
         raise NotImplementedError('Expected `step` to be implemented')
 
     @abstractmethod
-    def end(self, reward: float, extra: Dict[str, Any]) -> None:
+    def end(self, reward: float, extra: dict[str, Any]) -> None:
         raise NotImplementedError('Expected `end` to be implemented')
--- RlGlue/environment.py
+++ RlGlue/environment.py
@@ -1,12 +1,12 @@
 from abc import abstractmethod
-from typing import Any, Dict, Tuple
+from typing import Any, Protocol
 
 
-class BaseEnvironment:
+class BaseEnvironment(Protocol):
     @abstractmethod
     def start(self) -> Any:
         raise NotImplementedError('Expected `start` to be implemented')
 
     @abstractmethod
-    def step(self, action: int) -> Tuple[float, Any, bool, Dict[str, Any]]:
+    def step(self, action: int) -> tuple[Any, float, bool, bool, dict[str, Any]]:
         raise NotImplementedError('Expected `step` to be implemented')
